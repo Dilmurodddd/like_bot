9d356da38162e05b37f7bd0ef280f954b7cb2fc4
dilmurod <dilmurod@dilmurod.dilmurod>
2025-01-23 10:07:30 +0500

    like bot

---
+++ main.py
@@ -0,0 +1,32 @@
+from telegram import Update
+from telegram.ext import CallbackContext, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater
+import os
+likes = 0
+dislikes = 0
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text("Assalomu alaykum\n,men faqat like va deslikeni sanaydigan botman\n menga faqat like va deslike emojisini yuboring\nfaqat bir dona yuboring")
+def handle_message(update: Updater, context: CallbackContext):
+    global likes, dislikes
+    if update.message.text in "👍👍🏻👍🏼👍🏽👍🏾👍🏿":
+        likes += 1
+        update.message.reply_text(f"Like: {likes}\n"f"Dislike: {dislikes}")
+    elif update.message.text in "👎👎🏻👎🏼👎🏽👎🏾👎🏿":
+        dislikes += 1
+        update.message.reply_text(f"Like: {likes}\n"f"Dislike: {dislikes}")
+    else:
+        update.message.reply_text("Ming bora uzur so'rayman menga faqat 👎👎🏻👎🏼👎🏽👎🏾👎🏿 va 👍👍🏻👍🏼👍🏽👍🏾👍🏿 emojilarni qabul qilaman")
+def main():
+    TOKEN = os.getenv("TOKEN")  
+    updater = Updater(TOKEN)
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(Filters.text, handle_message))
+
+    updater.start_polling()
+    updater.idle()
+if __name__ == '__main__':
+    main() 
