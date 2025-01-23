46a6c41be6398dff5ee13e92620b77efce8ec336
dilmurod <dilmurod@dilmurod.dilmurod>
2025-01-23 10:33:55 +0500

    like bot

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ likes = 0
 dislikes = 0
 
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text("Assalomu alaykum\n,men faqat like va deslikeni sanaydigan botman\n menga faqat like va deslike emojisini yuboring\nfaqat bir dona yuboring")
+    update.message.reply_text("Assalomu alaykum\nmen faqat like va deslikeni sanaydigan botman\nmenga faqat like va deslike emojisini yuboring\nfaqat bir dona yuboring")
 def handle_message(update: Updater, context: CallbackContext):
     global likes, dislikes
     if update.message.text in "👍👍🏻👍🏼👍🏽👍🏾👍🏿":
         likes += 1
         update.message.reply_text(f"Like: {likes}\n"f"Dislike: {dislikes}")
+        
     elif update.message.text in "👎👎🏻👎🏼👎🏽👎🏾👎🏿":
         dislikes += 1
         update.message.reply_text(f"Like: {likes}\n"f"Dislike: {dislikes}")
+        
     else:
         update.message.reply_text("Ming bora uzur so'rayman menga faqat 👎👎🏻👎🏼👎🏽👎🏾👎🏿 va 👍👍🏻👍🏼👍🏽👍🏾👍🏿 emojilarni qabul qilaman")
 def main():
